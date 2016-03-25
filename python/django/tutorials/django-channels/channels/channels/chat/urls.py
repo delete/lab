28b31f9b5ec22e5e35da632a2721df5ed9f32650
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-03-25 14:52:46 -0300

    add views, urls, templates and static files

--- python/django/tutorials/django-channels/channels/channels/chat/urls.py
+++ python/django/tutorials/django-channels/channels/channels/chat/urls.py
@@ -1,3 +1,8 @@
 from django.conf.urls import url
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    url(r'^$',  views.about, name='about'),
+    url(r'^new/$', views.new_room, name='new_room'),
+    url(r'^(?P<label>[\w-]{,50})/$', views.chat_room, name='chat_room'),
+]
