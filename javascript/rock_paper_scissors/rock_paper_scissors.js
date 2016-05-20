8c161b49187918a67e47ffcf07ff5b271a28588e
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-05-20 16:17:04 -0300

    fix indentation

--- javascript/rock_paper_scissors/rock_paper_scissors.js
+++ javascript/rock_paper_scissors/rock_paper_scissors.js
@@ -1,88 +1,88 @@
 var CHOICES = {
-	'rock': 'images/rock.png',
-	'paper': 'images/paper.jpg',
-	'scissors': 'images/scissors.png'
+  'rock': 'images/rock.png',
+  'paper': 'images/paper.jpg',
+  'scissors': 'images/scissors.png'
 };
 
 (function () {
-	var chooseOptions = document.getElementById('options');
+  var chooseOptions = document.getElementById('options');
 
-	chooseOptions.addEventListener('click', function (event) {
-		var target = event.toElement || event.srcTarget || event.target || event.srcElement;
+  chooseOptions.addEventListener('click', function (event) {
+    var target = event.toElement || event.srcTarget || event.target || event.srcElement;
 
-		var playerChoice = target.id;
-		var computerChoice = getComputerChoice();
+    var playerChoice = target.id;
+    var computerChoice = getComputerChoice();
 
-		displayResults(playerChoice, computerChoice);
+    displayResults(playerChoice, computerChoice);
 
-		setTimeout(function(){
-			defaultEnv()
-		}, 1500);
-	});
+    setTimeout(function(){
+      defaultEnv()
+    }, 1500);
+  });
 }());
 
 function getComputerChoice () {
-	var options = ['rock', 'paper', 'scissors'];
-	return options[Math.floor(Math.random()*3)];
+  var options = ['rock', 'paper', 'scissors'];
+  return options[Math.floor(Math.random()*3)];
 };
 
 function displayResults (playerChoice, compChoice) {
-		
-		changeImages(playerChoice, compChoice);
-		
-		if ( playerChoice == compChoice ) {
-			setResultMessage('Tie!', 'tie');
+    
+    changeImages(playerChoice, compChoice);
+    
+    if ( playerChoice == compChoice ) {
+      setResultMessage('Tie!', 'tie');
 
-		} else if ( (playerChoice === 'rock' && compChoice === 'scissors') || (playerChoice === 'paper' && compChoice === 'rock') || (playerChoice === 'scissors' && compChoice === 'paper')) {
-			setResultMessage('You Win!', 'win');
+    } else if ( (playerChoice === 'rock' && compChoice === 'scissors') || (playerChoice === 'paper' && compChoice === 'rock') || (playerChoice === 'scissors' && compChoice === 'paper')) {
+      setResultMessage('You Win!', 'win');
 
-		} else {
-			setResultMessage('You Lose!', 'lose');
-		}
+    } else {
+      setResultMessage('You Lose!', 'lose');
+    }
 
 };
 
 function changeImages (playerChoice, compChoice) {
-	var playerHand = document.getElementById('right_hand');
-	playerHand.setAttribute('src', CHOICES[playerChoice]);
+  var playerHand = document.getElementById('right_hand');
+  playerHand.setAttribute('src', CHOICES[playerChoice]);
 
-	var compHand = document.getElementById('left_hand');
-	compHand.setAttribute('src', CHOICES[compChoice]);
+  var compHand = document.getElementById('left_hand');
+  compHand.setAttribute('src', CHOICES[compChoice]);
 };
 
 function setResultMessage (text, result) {
-	var message = document.getElementById('message');
+  var message = document.getElementById('message');
 
-	switch (result) {
-		case 'tie':
-			message.setAttribute('class', 'tie');
-			message.innerHTML = text;
-			break;
+  switch (result) {
+    case 'tie':
+      message.setAttribute('class', 'tie');
+      message.innerHTML = text;
+      break;
 
-		case 'win':
-			message.setAttribute('class', 'win');
-			message.innerHTML = text;
-			break;
+    case 'win':
+      message.setAttribute('class', 'win');
+      message.innerHTML = text;
+      break;
 
-		case 'lose':
-			message.setAttribute('class', 'lose');
-			message.innerHTML = text;
-			break;
-			
-		default:
-			message.setAttribute('class', '');
-	}
+    case 'lose':
+      message.setAttribute('class', 'lose');
+      message.innerHTML = text;
+      break;
+      
+    default:
+      message.setAttribute('class', '');
+  }
 };
 
 function defaultEnv () {
-	var playerHand = document.getElementById('right_hand');
-	playerHand.setAttribute('src', 'images/right_fist.png');
+  var playerHand = document.getElementById('right_hand');
+  playerHand.setAttribute('src', 'images/right_fist.png');
 
-	var compHand = document.getElementById('left_hand');
-	compHand.setAttribute('src', 'images/left_fist.png');
+  var compHand = document.getElementById('left_hand');
+  compHand.setAttribute('src', 'images/left_fist.png');
 
-	var message = document.getElementById('message');
-	message.innerHTML = 'Choose you option!';
-	message.setAttribute('class', '');
+  var message = document.getElementById('message');
+  message.innerHTML = 'Choose you option!';
+  message.setAttribute('class', '');
 };
 
