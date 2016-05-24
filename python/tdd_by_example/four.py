24f99976d97b3798d0f24959bc450df38b5708f7
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-05-24 10:11:48 -0300

    Invoke tearDown afterward

---
+++ python/tdd_by_example/four.py
@@ -0,0 +1,42 @@
+# Invoke tearDown afterward
+
+
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def run(self):
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
+        self.tearDown()
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def setUp(self):
+        self.log = "setUp "
+
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
+
+class TestCaseTest(TestCase):
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
+
+TestCaseTest("testTemplateMethod").run()
