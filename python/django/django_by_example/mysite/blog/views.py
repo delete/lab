8de2bf09ed69b230717181f0bcd874e5d2a7be41
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-06-02 10:34:45 -0300

    added tags functionality

--- python/django/django_by_example/mysite/blog/views.py
+++ python/django/django_by_example/mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -19,8 +19,27 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Commetn object byt don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assing the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post, 'comments': comments, 'comment_form': comment_form}
+    )
 
 
 def post_share(request, post_id):
