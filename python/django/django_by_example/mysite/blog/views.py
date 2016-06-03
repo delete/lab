3852b1a62daf34c48706328eb7d026d618f06a64
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-06-03 10:44:54 -0300

    add related post by tags

--- python/django/django_by_example/mysite/blog/views.py
+++ python/django/django_by_example/mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from django.core.paginator import (
     Paginator, EmptyPage, PageNotAnInteger
 )
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -33,10 +34,21 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    # values_list() without flat parameter, return  a list of tuples
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post, 'comments': comments, 'comment_form': comment_form}
+        {
+            'post': post, 'comments': comments,
+            'comment_form': comment_form, 'similar_posts': similar_posts
+        }
     )
 
 
