d096808f60d024b076b5f51e625421b8e054993f
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-06-06 09:31:08 -0300

    change password views

--- python/django/django_by_example/bookmarks/account/urls.py
+++ python/django/django_by_example/bookmarks/account/urls.py
@@ -11,4 +11,13 @@ urlpatterns = [
         name='logout_then_login'
     ),
     url(r'^$', views.dashboard, name='dashboard'),
+    url(
+        r'^password-change/$', 'django.contrib.auth.views.password_change',
+        name='password_change'
+    ),
+    url(
+        r'^password-change/done/$',
+        'django.contrib.auth.views.password_change_done',
+        name='password_change_done'
+    ),
 ]
