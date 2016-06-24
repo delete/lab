a12fef4e0a139287e665f79f495c492ac8c057a8
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-06-24 08:47:46 -0300

    language prefix to URL patterns

--- python/django/django_by_example/myshop/orders/urls.py
+++ python/django/django_by_example/myshop/orders/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
+from django.utils.translation import gettext_lazy as _
 
 from . import views
 
 
 urlpatterns = [
-    url(r'^create/$', views.order_create, name='order_create'),
+    url(_(r'^create/$'), views.order_create, name='order_create'),
     url(
         r'^admin/order/(?P<order_id>\d+)/$',
         views.admin_order_detail,
